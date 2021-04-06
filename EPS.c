7b2532868719461c555d8126865b09a03cf38010
mairacanal <maira.canal@usp.br>
2021-04-05 21:39:41 -0300

    adding health checks to EPS

--- EPS.c
+++ EPS.c
@@ -1,5 +1,6 @@
 #include <pthread.h>
 #include <unistd.h>
+#include <sys/select.h>
 #include "can.h"
 
 struct systemArgs {
@@ -8,20 +9,24 @@ struct systemArgs {
     struct can_frame *frame;
 };
 
+void healthCheck(int file_descriptor, pthread_mutex_t *mutex);
 void *systemTransmit(void *args);
 void *systemReceive(void *args);
 
 int main () {
 
-    int s, rc;
+    int s;
     struct can_frame transmitFrame;
     pthread_mutex_t systemCommunicationMutex = PTHREAD_MUTEX_INITIALIZER;
+
+    socket_initiation("vcan0", &s);
+    healthCheck(s, &systemCommunicationMutex);
+
+    int rc;
     pthread_t systemThread;
     struct systemArgs *transmitArgs = (struct systemArgs *)malloc(sizeof(struct systemArgs));
     struct systemArgs *receiveArgs = (struct systemArgs *)malloc(sizeof(struct systemArgs));
-
-    socket_initiation("vcan0", &s);
-
+    
     transmitArgs->file_descriptor = s;
     transmitArgs->mutex = &systemCommunicationMutex;
     transmitArgs->frame = &transmitFrame;
@@ -31,10 +36,39 @@ int main () {
         printf("Thread creation failed: %d\n", rc);
 
     }
-
+    
     pthread_join(systemThread, NULL);
 
     socket_close(s);
+
+}
+
+void healthCheck(int file_descriptor, pthread_mutex_t *mutex) {
+
+    struct can_frame frame;
+
+    pthread_mutex_lock(mutex);
+
+    socket_read(file_descriptor, &frame);
+    printCANframe(frame);
+
+    if (frame.can_id == 0x01) {
+
+        frame.can_id = 0x7F;
+        frame.can_dlc = 1;
+        frame.data[0] = 0x01; // Here, the system makes tests and return if the EPS is OK
+
+        socket_write(file_descriptor, &frame);
+
+        frame.can_id = 0x8F;
+        frame.can_dlc = 1;
+        frame.data[0] = 0x01; // Here, the system makes tests and return if the SOLAR is OK    
+
+        socket_write(file_descriptor, &frame);
+
+    }
+
+    pthread_mutex_unlock(mutex);
 
 }
 
@@ -69,9 +103,9 @@ void *systemReceive(void *args) {
         socket_read(systemArgs->file_descriptor, systemArgs->frame);
         printCANframe(*(systemArgs->frame));
         
-        if (systemArgs->frame->can_id == 0x01) {
+        if (systemArgs->frame->can_id == 0x02) {
 
-            systemArgs->frame->can_id = 0x02;   
+            systemArgs->frame->can_id = 0x03;   
             systemArgs->frame->can_dlc = 1;
             systemArgs->frame->data[0] = 0x4B;
             
