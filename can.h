df0e238d2f5c174ccded270ab2a034297f028fbc
mairacanal <maira.canal@usp.br>
2021-04-09 19:41:26 -0300

    fixing header files

--- can.h
+++ can.h
@@ -1,3 +1,6 @@
+#ifndef LINUX_CAN_H
+#define LINUX_CAN_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -10,16 +13,13 @@
 #include <linux/can.h>
 #include <linux/can/raw.h>
 
-#ifdef LINUX_CAN_H
-#define LINUX_CAN_H
-
 int socket_initiation(char *, int *);
 
 int socket_read(int, struct can_frame *);
 
 int socket_write (int, struct can_frame *);
 
-void printCANframe (can_frame);
+void printCANframe (struct can_frame);
 
 int socket_close(int);
 
